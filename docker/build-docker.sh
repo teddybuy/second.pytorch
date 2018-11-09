eb130969949dc3f52e90d48a2ab83622269b9791
zheng <teddybuybuy@gmail.com>
2018-11-08 18:41:48 -0800

    add set -e

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,3 +1,4 @@
+set -e
 nvidia-docker build -f Dockerfile-python36-pytorch41-second -t python36-pytorch41-second-step1 .
 NV_GPU=0 nvidia-docker run python36-pytorch41-second-step1 bash -c "cd /SparseConvNet && git checkout b596b10752136cd805b3b3f0507ac2a2e2d414fc && chmod +x build.sh && ./build.sh && cd .."
 export CONTAINER_ID=`docker ps -lq`
